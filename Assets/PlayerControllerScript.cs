ac96aa997eda3b287d32e8ce540285feac580058
Jeff Souza <jeffreymsouza@gmail.com>
2016-03-27 20:45:25 -0600

    Starting first level

---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    bool inCombat = false;
+    float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter2D()
+    {
+        inCombat = true;
+        rb.velocity = new Vector2(3 * speed, -10f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if(!inCombat)
+        {
+            float move = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(move * speed, 0);
+            animator.SetFloat("Speed", move * speed);
+        }        
+	}
+}
---
+++ Assets/word_generator_script.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class word_generator_script : MonoBehaviour {
+
+    public GameObject wordPrefab;
+    public float spawnTime = 0.5f;
+    public bool left = true;
+
+    public float yMin = 0f;
+    public float yMax = 20f;
+    public float xLeft = -10f;
+    public float xRight = 10f;
+    public float speed = 10f;
+
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+	
+	void Spawn () {
+        Vector2 spawnPos;
+        float velocity;
+        if (left)
+        {
+            velocity = speed;
+            spawnPos = new Vector2(xLeft, Random.Range(yMin, yMax));
+        }
+        else
+        {
+            spawnPos = new Vector2(xRight, Random.Range(yMin, yMax));
+            velocity = -speed;
+        }
+        GameObject word = Instantiate(wordPrefab, spawnPos, Quaternion.identity) as GameObject;
+        Rigidbody2D rb = word.GetComponent<Rigidbody2D>();
+
+        TextMesh tm = word.GetComponent<TextMesh>();
+        tm.text = "Sup";
+
+        rb.velocity = new Vector2(velocity*3, 0);
+
+        left = !left;
+	}
+}
