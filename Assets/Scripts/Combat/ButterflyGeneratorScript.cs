f8e08875a30204c5dbc72e2fd359dcdd29056b4f
Jeff Souza <jeffreymsouza@gmail.com>
2016-04-18 12:14:25 -0600

    updated butterfly colors, animation. updated house art.

--- Assets/Scripts/Combat/ButterflyGeneratorScript.cs
+++ Assets/Scripts/Combat/ButterflyGeneratorScript.cs
@@ -20,14 +20,28 @@ public class ButterflyGeneratorScript : MonoBehaviour {
         Vector3 spawn = gameObject.GetComponent<Rigidbody2D>().position;
         spawn.z = 20;
         GameObject word = Instantiate(ButterflyPrefab, spawn, Quaternion.identity) as GameObject;
+
+        float c = Random.Range(0, 4);
+        Animator a = word.GetComponent<Animator>();
+        if (c < 1)
+            a.Play("green");        
+        else if (c < 2)
+            a.Play("blue");        
+        else if (c < 3)
+            a.Play("yellow");
+        else if (c < 4)
+            a.Play("pink");
+
         butterflies.Add(word);
     }
 
     public void Despawn ()
     {
+        /*
         foreach(GameObject b in butterflies)
         {
             Destroy(b);
         }
+        */
     }
 }
